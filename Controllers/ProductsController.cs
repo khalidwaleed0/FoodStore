dc523bc1b403bda8311ef9c7940a02301b646312
HebaMohey <105401747+HebaMohey@users.noreply.github.com>
2022-09-12 02:15:03 +0200

    scafolding

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FoodStore.Models;
+
+namespace FoodStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly SouqcomContext db=new SouqcomContext();
+
+     
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var souqcomContext = db.Products.Include(p => p.Cat);
+            return View(await souqcomContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || db.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,Catid,Photo,Type,SupplierName,EntryDate,ReviewUrl,Quantity,Priceafterdiscount")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || db.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Catid,Photo,Type,SupplierName,EntryDate,ReviewUrl,Quantity,Priceafterdiscount")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(product);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Catid"] = new SelectList(db.Categories, "Id", "Id", product.Catid);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || db.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (db.Products == null)
+            {
+                return Problem("Entity set 'SouqcomContext.Products'  is null.");
+            }
+            var product = await db.Products.FindAsync(id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+            }
+            
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (db.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Models/Review.cs
+++ Models/Review.cs
@@ -6,9 +6,9 @@ namespace FoodStore.Models
     public partial class Review
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Subject { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
         public string? Description { get; set; }
     }
 }
